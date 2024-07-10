d8751f8bb51bced95fec939bdcd44933e77fb357
Tobias Simetsreiter <dasimmet@gmail.com>
2024-07-11 01:06:59 +0200

    add options

--- cmakebuild.sh
+++ cmakebuild.sh
@@ -20,11 +20,19 @@ trap cleanup EXIT
 mkdir -p build
 cd build
 CMAKE_OPTIONS="
-    -DREGEX_BACKEND=pcre2
+    -DREGEX_BACKEND=builtin
     -DDEPRECATE_HARD=ON
     -DCMAKE_BUILD_TYPE=Debug
+    -DUSE_HTTPS=OpenSSL
+    -DUSE_GSSAPI=ON
+    -DDEBUG_STRICT_ALLOC=ON
+    -DDEBUG_STRICT_OPEN=ON
     -DUSE_SSH=exec"
-export CMAKE_GENERATOR=Ninja
 
+# -DUSE_THREADS=OFF
+# -DUSE_LEAK_CHECKER=valgrind
+
+export CMAKE_GENERATOR=Ninja
+export PKGCONFIG="false"
 cmake .. $CMAKE_OPTIONS 2>&1 | tee cmake.log
 cmake --build . 2>&1 | tee -a cmake.log
